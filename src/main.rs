f9408ee891965c5b8d4b1ba82fcc60ceb8296c8e
Jörg Thalheim <joerg@thalheim.io>
2019-02-12 23:28:06 +0000

    switch to tmpfs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use nix::mount::{mount, MsFlags};
 use nix::sched::{unshare, CloneFlags};
-use nix::sys::signal::{kill, Signal};
-use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd;
-use nix::unistd::{fork, ForkResult};
 use std::env;
 use std::fs;
 use std::io;
@@ -63,17 +60,34 @@ fn run_chroot(nixdir: &Path, rootdir: &Path, cmd: &str, args: &[String]) {
 
     unshare(CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUSER).expect("unshare failed");
 
+    // fixes issue #1 where writing to /proc/self/gid_map fails
+    // see user_namespaces(7) for more documentation
+    if let Ok(mut file) = fs::File::create("/proc/self/setgroups") {
+        let _ = file.write_all(b"deny");
+    }
+
+    let mut uid_map =
+        fs::File::create("/proc/self/uid_map").expect("failed to open /proc/self/uid_map");
+    uid_map
+        .write_all(format!("{} {} 1", uid, uid).as_bytes())
+        .expect("failed to write new uid mapping to /proc/self/uid_map");
+
+    let mut gid_map =
+        fs::File::create("/proc/self/gid_map").expect("failed to open /proc/self/gid_map");
+    gid_map
+        .write_all(format!("{} {} 1", gid, gid).as_bytes())
+        .expect("failed to write new gid mapping to /proc/self/gid_map");
+
     // prepare pivot_root call:
     // rootdir must be a mount point
-
+    let opts = format!("size=1M,mode=700,nr_inodes=10k,uid={},gid={}", uid, gid);
     mount(
-        Some(rootdir),
+        Some("tmpfs"),
         rootdir,
-        Some("none"),
-        MsFlags::MS_BIND | MsFlags::MS_REC,
-        NONE,
-    )
-    .expect("failed to re-bind mount / to our new chroot");
+        Some("tmpfs"),
+        MsFlags::empty(),
+        Some(opts.as_str()),
+    ).expect("failed to mount tmpfs for new chroot");
 
     mount(
         Some(rootdir),
@@ -81,8 +95,7 @@ fn run_chroot(nixdir: &Path, rootdir: &Path, cmd: &str, args: &[String]) {
         Some("none"),
         MsFlags::MS_PRIVATE | MsFlags::MS_REC,
         NONE,
-    )
-    .expect("failed to re-mount our chroot as private mount");
+    ).expect("failed to make tmpfs chroot a private mount");
 
     // create the mount point for the old root
     // The old root cannot be unmounted/removed after pivot_root, the only way to
@@ -90,7 +103,7 @@ fn run_chroot(nixdir: &Path, rootdir: &Path, cmd: &str, args: &[String]) {
     // we pivot the old root to /nix. This is somewhat confusing, though.
     let nix_mountpoint = rootdir.join("nix");
     fs::create_dir(&nix_mountpoint)
-        .unwrap_or_else(|_| panic!("failed to create {}/nix", &nix_mountpoint.display()));
+        .unwrap_or_else(|_| panic!("failed to create {}", &nix_mountpoint.display()));
 
     unistd::pivot_root(rootdir, &nix_mountpoint).unwrap_or_else(|_| {
         panic!(
@@ -121,79 +134,17 @@ fn run_chroot(nixdir: &Path, rootdir: &Path, cmd: &str, args: &[String]) {
     )
     .unwrap_or_else(|_| panic!("failed to bind mount {} to /nix", nix_store.display()));
 
-    // fixes issue #1 where writing to /proc/self/gid_map fails
-    // see user_namespaces(7) for more documentation
-    if let Ok(mut file) = fs::File::create("/proc/self/setgroups") {
-        let _ = file.write_all(b"deny");
-    }
-
-    let mut uid_map =
-        fs::File::create("/proc/self/uid_map").expect("failed to open /proc/self/uid_map");
-    uid_map
-        .write_all(format!("{} {} 1", uid, uid).as_bytes())
-        .expect("failed to write new uid mapping to /proc/self/uid_map");
-
-    let mut gid_map =
-        fs::File::create("/proc/self/gid_map").expect("failed to open /proc/self/gid_map");
-    gid_map
-        .write_all(format!("{} {} 1", gid, gid).as_bytes())
-        .expect("failed to write new gid mapping to /proc/self/gid_map");
-
     // restore cwd
     env::set_current_dir(&cwd)
         .unwrap_or_else(|_| panic!("cannot restore working directory {}", cwd.display()));
 
-    let err = process::Command::new(cmd)
+    let err = process::Command::new(&cmd)
         .args(args)
         .env("NIX_CONF_DIR", "/nix/etc/nix")
         .exec();
 
-    eprintln!("failed to execute {}: {}", &args[2], err);
+    eprintln!("failed to execute {}: {}", &cmd, err);
     process::exit(1);
-}
-
-fn wait_for_child(child_pid: unistd::Pid, tempdir: TempDir, rootdir: &Path) {
-    loop {
-        match waitpid(child_pid, Some(WaitPidFlag::WUNTRACED)) {
-            Ok(WaitStatus::Signaled(child, Signal::SIGSTOP, _)) => {
-                let _ = kill(unistd::getpid(), Signal::SIGSTOP);
-                let _ = kill(child, Signal::SIGCONT);
-            }
-            Ok(WaitStatus::Signaled(_, signal, _)) => {
-                kill(unistd::getpid(), signal)
-                    .unwrap_or_else(|_| panic!("failed to send {} signal to our self", signal));
-            }
-            Ok(WaitStatus::Exited(_, status)) => {
-                tempdir.close().unwrap_or_else(|_| {
-                    panic!(
-                        "failed to remove temporary directory: {}",
-                        rootdir.display()
-                    )
-                });
-                process::exit(status);
-            }
-            Ok(what) => {
-                tempdir.close().unwrap_or_else(|_| {
-                    panic!(
-                        "failed to remove temporary directory: {}",
-                        rootdir.display()
-                    )
-                });
-                eprintln!("unexpected wait event happend: {:?}", what);
-                process::exit(1);
-            }
-            Err(e) => {
-                tempdir.close().unwrap_or_else(|_| {
-                    panic!(
-                        "failed to remove temporary directory: {}",
-                        rootdir.display()
-                    )
-                });
-                eprintln!("waitpid failed: {}", e);
-                process::exit(1);
-            }
-        };
-    }
 }
 
 fn main() {
@@ -209,11 +160,5 @@ fn main() {
     let nixdir = fs::canonicalize(&args[1])
         .unwrap_or_else(|_| panic!("failed to resolve nix directory {}", &args[1]));
 
-    match fork() {
-        Ok(ForkResult::Parent { child, .. }) => wait_for_child(child, tempdir, &rootdir),
-        Ok(ForkResult::Child) => run_chroot(&nixdir, &rootdir, &args[2], &args[3..]),
-        Err(e) => {
-            eprintln!("fork failed: {}", e);
-        }
-    };
+    run_chroot(&nixdir, &rootdir, &args[2], &args[3..]);
 }
